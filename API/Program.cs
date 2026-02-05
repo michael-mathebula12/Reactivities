e9c598bc772d09dfc94016761d6b718a8f673c64
michael mathebula <MMathebula@singular.co.za>
2026-02-05 11:02:31 +0200

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,16 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>( opt =>
+builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 
+builder.Services.AddCors();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 
 app.MapControllers();
 
@@ -27,14 +34,14 @@ var services = scope.ServiceProvider;
 try
 {
     //allows us to query the db
-    var context = services.GetRequiredService<AppDbContext>(); 
+    var context = services.GetRequiredService<AppDbContext>();
     await context.Database.MigrateAsync();
     await DbInitializer.SeedData(context);
 }
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error occured during migration.");
+    logger.LogError(ex, "An error occured during migration.");
 }
 
 app.Run();
