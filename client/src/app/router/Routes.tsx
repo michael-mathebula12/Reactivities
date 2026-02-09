856c7317dcf4b51bebe8aacd013b0f5ced3b8473
michael mathebula <MMathebula@singular.co.za>
2026-02-09 12:02:23 +0200

    end of section 9

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,6 +1,7 @@
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetailPage from "../../features/activities/details/ActivityDetailPage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import Counter from "../../features/counter/Counter";
 import HomePage from "../../features/home/HomePage";
 import App from "../layout/App";
 import { createBrowserRouter } from "react-router";
@@ -13,8 +14,10 @@ export const router = createBrowserRouter([
             { path: '', element: <HomePage /> },
             { path: 'activities', element: <ActivityDashboard /> },
             { path: 'activities/:id', element: <ActivityDetailPage /> },
-            { path: 'createActivity', element: <ActivityForm key='create'/> },
-            { path: 'manage/:id', element: <ActivityForm /> }
+            { path: 'createActivity', element: <ActivityForm key='create' /> },
+            { path: 'manage/:id', element: <ActivityForm /> },
+            { path: 'counter', element: <Counter /> },
+
 
 
         ]
